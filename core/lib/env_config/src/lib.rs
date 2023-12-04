a292d2eb6bf20500a847338df0fd18c3543533a3
juan.mv <juan@lambdaclass.com>
2023-12-04 12:15:18 -0300

    Remove dotenv

--- core/lib/env_config/src/lib.rs
+++ core/lib/env_config/src/lib.rs
@@ -1,6 +1,5 @@
 use anyhow::Context as _;
 use serde::de::DeserializeOwned;
-use dotenv::dotenv;
 
 mod alerts;
 mod api;
@@ -37,7 +36,6 @@ pub trait FromEnv: Sized {
 /// Convenience function that loads the structure from the environment variable given the prefix.
 /// Panics if the config cannot be loaded from the environment variables.
 pub(crate) fn envy_load<T: DeserializeOwned>(name: &str, prefix: &str) -> anyhow::Result<T> {
-    dotenv().ok();
     envy::prefixed(prefix)
         .from_env()
         .with_context(|| format!("Cannot load config <{name}>"))
